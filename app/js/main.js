79bc8b25f7ef7f2d3afe1f22cfada932adc830cb
DjekShot <djek_stepanov@mail.ru>
2021-05-29 17:45:41 +0500

    added products and producta-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 3.6,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    $('.product-slider-inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false,
+    });
     
+    var mixer = mixitup('.products__inner-box');
 
 });
